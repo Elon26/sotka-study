885d85f9957dd7b2de093f008a541818a99275bc
Evgeniy Sologub <evgeniy.sologub.vl@gmail.com>
2023-07-13 13:23:46 +1000

    upload

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -3,15 +3,15 @@ import styles from "./styles.module.scss";
 export default function MainPage(): React.ReactElement {
     return (
         <div className="container">
-            <p>Главная страница</p>
+            <p>Главная страница!!!</p>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <p>Главная страница</p>
+            <p>Главная страница!!!</p>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <p>Главная страница</p>
+            <p>Главная страница!!!</p>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <p>Главная страница</p>
+            <p>Главная страница!!!</p>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <p>Главная страница</p>
+            <p>Главная страница!!!</p>
         </div>
     );
 }
