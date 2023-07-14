3ddc57f7163d97e513e6ecaa04e1132551e8e720
Evgeniy Sologub <evgeniy.sologub.vl@gmail.com>
2023-07-15 08:52:18 +1000

    upload

--- src/components/pages/MainPage/GiftPicture/GiftPicture.tsx
+++ src/components/pages/MainPage/GiftPicture/GiftPicture.tsx
@@ -1,8 +1,7 @@
 import styles from "./styles.module.scss";
 import { githubHomePageUrl } from "../../../../data/contants.json";
 import Image from "next/image";
-// import ststsImg from "https://elon26.github.io/sotka-study/images/main-page/stats.webp";
-import ststsImg from "public/images/main-page/stats.webp";
+import statsImg from "public/images/main-page/stats.webp";
 
 export default function GiftPicture(): React.ReactElement {
     return (
@@ -10,8 +9,7 @@ export default function GiftPicture(): React.ReactElement {
             <div className={styles.wrapper}>
                 <Image
                     className={styles.image}
-                    // src={githubHomePageUrl + "https://elon26.github.io/sotka-study/images/main-page/stats.webp"}
-                    src={ststsImg}
+                    src={statsImg}
                     // srcSet
                     alt="график"
                     width={1920}
